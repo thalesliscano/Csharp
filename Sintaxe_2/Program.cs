1a583d3eef67c08eb498b46ef6e5cd8084f3cbdb
Thales <thalesdliscano@gmail.com>
2023-09-14 17:25:58 -0400

    add-outros-exercicios

---
+++ Sintaxe_2/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Implemente um programa que simule o funcionamento de uma caixa registradora. Utilize uma pilha para representar os itens comprados e uma fila para representar as ordens de compra. O programa deve permitir adicionar itens à pilha, remover itens da pilha (representando a venda) e adicionar ordens de compra à fila. Exiba os itens vendidos e as ordens de compra à medida que forem processados.|*/
+
+Console.WriteLine("Estamos em um mercado, escolha uns itens abaixo");
+Console.WriteLine("Arroz - [1]");
+Console.WriteLine("Feijão -[2]");
+Console.WriteLine("Carne - [3]");
+Console.WriteLine("Sprit - [4]");
+Console.WriteLine("Oleo -  [5]");
+Console.WriteLine("-----------------------------------------------------");
+
+Console.WriteLine("Faça suas compras, mas antes...");
+Console.WriteLine("Quantos itens vai comprar: ");
+int quantidadeEscolha = int.Parse(Console.ReadLine());
+Stack<string> listaCompras = new Stack<string>();
+
+for(var i = 1; i <= quantidadeEscolha;i++) {
+  Console.WriteLine("Pegue seus itens:");
+
+  int escolha = int.Parse(Console.ReadLine());
+  string escolhaItem = "";
+
+  switch (escolha) {
+    case 1:
+      escolhaItem= "Arroz";
+      break;
+    case 2:
+      escolhaItem = "Feijão";
+      break;
+    case 3:
+      escolhaItem = "Carne";
+      break;
+    case 4:
+      escolhaItem = "Sprit";
+      break;
+    case 5:
+      escolhaItem = "Oleo";
+      break;
+    default:
+      Console.WriteLine("Opção inválida");
+      continue;
+  }
+  listaCompras.Push(escolhaItem);
+}
+
+Queue<string> listaComprasQueue = new Queue<string>();
+
+Console.WriteLine("Gostaria de remover algum item?")
+Console.WriteLine("Quantos itens quer remover: ") 
+int quantidadeParaRemover = int.Parse(Console.ReadLine()); // 3
+for(var i = 1;i <= quantidadeParaRemover;i++) // i = 1 qRomov = 3
+
+{
+  int remover = int.Parse(Console.ReadLine());
+  string removerItem = "";
+
+  switch (remover) {
+    case 1:
+      removerItem = "Arroz";
+      break;
+    case 2:
+      removerItem = "Feijão";
+      break;
+    case 3:
+      removerItem = "Carne";
+      break;
+    case 4:
+      removerItem = "Sprit";
+      break;
+    case 5:
+      removerItem = "Oleo";
+      break;
+    default:
+      Console.WriteLine("Opção inválida");
+      continue;
+  }
+
+}
+foreach(var compras in listaCompras){
+  Console.WriteLine($"{contador}° item: {compras.ToUpper()}");  
+};
+  
+
+
+
+
---
+++ Sintaxe_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<string> pilha = new Stack<string>();
+
+        pilha.Push("Primeiro");
+        pilha.Push("Segundo");
+        pilha.Push("Terceiro");
+
+        // Transferir os elementos da pilha para uma lista
+        List<string> lista = pilha.ToList();
+
+        // Remover o elemento mais antigo da lista
+        string elementoRemovido = lista[0];
+        lista.RemoveAt(0);
+
+        // Transferir os elementos de volta para a pilha (caso deseje manter a pilha)
+        pilha.Clear();
+        foreach (var elemento in lista)
+        {
+            pilha.Push(elemento);
+        }
+
+        Console.WriteLine("Elemento mais antigo removido: " + elementoRemovido); // Isso imprimirá "Elemento mais antigo removido: Primeiro"
+    }
+}
+
